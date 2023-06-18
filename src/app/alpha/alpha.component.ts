199cdb2c2b9a53c345ac0dbaa7a61c06dcb3c8ba
Prasanna Raja <prasannaraja@msn.com>
2023-06-18 20:29:36 +0200

    Update alpha.component.ts

--- src/app/alpha/alpha.component.ts
+++ src/app/alpha/alpha.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService } from "../services/data.service";
-import { Observable } from "rxjs";
-import { IPersonalData } from "../models/IPersonalData";
+import { BehaviorSubject, Observable, map, switchMap } from "rxjs";
+import { Contact, IPersonalData } from "../models/IPersonalData";
+import { Company } from "../models/ICompanies";
 
 @Component({
   selector: "app-alpha",
@@ -10,12 +11,34 @@ import { IPersonalData } from "../models/IPersonalData";
   styleUrls: ["./alpha.component.scss"],
 })
 export class AlphaComponent implements OnInit {
-  constructor(public dataService: DataService) {}
+  private contactSubject: BehaviorSubject<Contact[]>;
+  constructor(public dataService: DataService) {
+    this.contactSubject = new BehaviorSubject<Contact[]>([]);
+  }
+  get contacts(): Contact[] {
+    return this.contactSubject.value;
+  }
+
   ngOnInit(): void {
-    this.personalData$.subscribe((p) => console.log(p));
+    this.personalData$
+      .pipe(
+        map((p) => {
+          this.contactSubject.next(p.contact);
+        }),
+        switchMap((e) => {
+          return this.experienceData$;
+        })
+      )
+      .subscribe((e) => {
+        console.log(e);
+      });
   }
 
   get personalData$(): Observable<IPersonalData> {
     return this.dataService.getPersonalData();
+  }
+
+  get experienceData$(): Observable<Company[]> {
+    return this.dataService.getExperienceData();
   }
 }
