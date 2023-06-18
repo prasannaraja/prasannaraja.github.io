5dcd1e8a168a33037c8fa8a225e311f950773bf5
Prasanna Raja <prasannaraja@msn.com>
2023-06-18 21:16:14 +0200

    Update alpha.component.ts

--- src/app/alpha/alpha.component.ts
+++ src/app/alpha/alpha.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService } from "../services/data.service";
-import { BehaviorSubject, Observable, map, switchMap } from "rxjs";
-import { Contact, IPersonalData } from "../models/IPersonalData";
+import { Observable, map, switchMap } from "rxjs";
+import { IPersonalData } from "../models/IPersonalData";
 import { Company } from "../models/ICompanies";
+import { AlphaComponentBase } from "./base/alpha.component.base";
 
 @Component({
   selector: "app-alpha",
@@ -10,35 +11,46 @@ import { Company } from "../models/ICompanies";
   templateUrl: "./alpha.component.html",
   styleUrls: ["./alpha.component.scss"],
 })
-export class AlphaComponent implements OnInit {
-  private contactSubject: BehaviorSubject<Contact[]>;
+export class AlphaComponent extends AlphaComponentBase implements OnInit {
   constructor(public dataService: DataService) {
-    this.contactSubject = new BehaviorSubject<Contact[]>([]);
-  }
-  get contacts(): Contact[] {
-    return this.contactSubject.value;
-  }
-
-  ngOnInit(): void {
-    this.personalData$
-      .pipe(
-        map((p) => {
-          this.contactSubject.next(p.contact);
-        }),
-        switchMap((e) => {
-          return this.experienceData$;
-        })
-      )
-      .subscribe((e) => {
-        console.log(e);
-      });
+    super();
   }
 
   get personalData$(): Observable<IPersonalData> {
-    return this.dataService.getPersonalData();
+    return this.dataService.getPersonalData().pipe(
+      map((p) => {
+        this.contactsSubject$.next(p.contacts);
+        this.skillsSubject$.next(p.skills);
+        this.referencesSubject$.next(p.references);
+        this.educationsSubject$.next(p.educations);
+        this.firstNameSubject$.next(p.firstName);
+        this.lastNameSubject$.next(p.lastName);
+        this.jobTitleSubject$.next(p.jobTitle);
+        this.locationSubject$.next(p.location);
+        this.githubProfileSubject$.next(p.githubProfile);
+        return p;
+      })
+    );
   }
 
   get experienceData$(): Observable<Company[]> {
-    return this.dataService.getExperienceData();
+    return this.dataService.getExperienceData().pipe(
+      map((c) => {
+        this.companiesSubject$.next(c);
+        return c;
+      })
+    );
+  }
+
+  get getData$() {
+    return this.personalData$.pipe(
+      switchMap(() => {
+        return this.experienceData$;
+      })
+    );
+  }
+
+  ngOnInit(): void {
+    this.getData$.subscribe();
   }
 }
