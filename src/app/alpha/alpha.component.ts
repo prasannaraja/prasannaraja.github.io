a8c911a26e78bbc6b29be48512ec2f94c8210507
Prasanna Raja <prasannaraja@msn.com>
2023-06-18 18:17:20 +0200

    Update alpha.component.ts

--- src/app/alpha/alpha.component.ts
+++ src/app/alpha/alpha.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../services/data.service";
+import { Observable } from "rxjs";
+import { IPersonalData } from "../models/IPersonalData";
 
 @Component({
-  selector: 'app-alpha',
-  templateUrl: './alpha.component.html',
-  styleUrls: ['./alpha.component.scss']
+  selector: "app-alpha",
+  providers: [DataService],
+  templateUrl: "./alpha.component.html",
+  styleUrls: ["./alpha.component.scss"],
 })
-export class AlphaComponent {
+export class AlphaComponent implements OnInit {
+  constructor(public dataService: DataService) {}
+  ngOnInit(): void {
+    this.personalData$.subscribe((p) => console.log(p));
+  }
 
+  get personalData$(): Observable<IPersonalData> {
+    return this.dataService.getPersonalData();
+  }
 }
