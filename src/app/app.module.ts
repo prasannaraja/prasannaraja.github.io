8e5c82fa1df45ba131e05bd79fc5222cb77eb577
Prasanna Raja <prasannaraja@msn.com>
2023-06-18 22:21:43 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 import { AlphaComponent } from "./alpha/alpha.component";
 import { DataService } from "./services/data.service";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { RemoveHttpPrefixPipe } from "./pipe/RemoveHttpPrefixPipe";
 
 @NgModule({
-  declarations: [AppComponent, AlphaComponent],
+  declarations: [AppComponent, AlphaComponent, RemoveHttpPrefixPipe],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [DataService, HttpClient],
   bootstrap: [AppComponent],
