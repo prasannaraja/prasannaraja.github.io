1b1ce650bed3f53456d62328e21b41d913e5bc0d
Prasanna Raja <prasannaraja@msn.com>
2023-06-18 18:17:22 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AlphaComponent } from './alpha/alpha.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AlphaComponent } from "./alpha/alpha.component";
+import { DataService } from "./services/data.service";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AlphaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AlphaComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [DataService, HttpClient],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
