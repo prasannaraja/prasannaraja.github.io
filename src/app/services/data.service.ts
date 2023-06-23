da301957298350edd81050563011041c8663b41e
Prasanna Raja Prabhakaran <PrasannaRaja.Prabhakaran@kpmg.co.uk>
2023-06-23 15:54:41 +0200

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { IPersonalData } from "../models/IPersonalData";
-import { Company } from "../models/ICompanies";
+import { Company, ICompanies } from "../models/ICompanies";
 
 @Injectable({
   providedIn: "root",
 })
 export class DataService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   public getPersonalData(): Observable<IPersonalData> {
     return this.http.get<IPersonalData>("assets/sample.personal.data.json");
   }
 
-  public getExperienceData(): Observable<Company[]> {
-    return this.http.get<Company[]>("assets/sample.experience.data.json");
+  public getExperienceData(): Observable<ICompanies> {
+    return this.http.get<ICompanies>("assets/sample.experience.data.json");
   }
 }
