dfe42c7e4a60052b2608c32be48fc1a8a91dac2e
Prasanna Raja <prasannaraja@msn.com>
2023-06-27 00:06:49 +0200

    prettierrc changes

--- apps/resume-builder-x/src/app/generate/src/lib/lib.routes.ts
+++ apps/resume-builder-x/src/app/generate/src/lib/lib.routes.ts
@@ -1,8 +1,11 @@
 import { Route } from '@angular/router';
 
 export const generateRoutes: Route[] = [
-  {
-    path: 'generate',
-    loadChildren: () => import('./generate.route.module').then(m => m.GenerateRoutingModule)
-  }
-]
+    {
+        path: 'generate',
+        loadChildren: () =>
+            import('./generate.route.module').then(
+                (m) => m.GenerateRoutingModule
+            ),
+    },
+];
--- apps/resume-builder-x/src/app/generate/src/models/IPersonalData.ts
+++ apps/resume-builder-x/src/app/generate/src/models/IPersonalData.ts
@@ -1,57 +1,61 @@
 export interface IPersonalData {
-  firstName: string;
-  lastName: string;
-  contacts: Contact[];
-  githubProfile: string;
-  profileSummary: string;
-  location: string;
-  jobTitle: string;
-  skills: SkillGroup[];
-  educations: Education[];
-  references: Reference[];
+    firstName: string;
+    lastName: string;
+    contacts: Contact[];
+    githubProfile: string;
+    profileSummary: string;
+    location: string;
+    jobTitle: string;
+    skills: SkillGroup[];
+    educations: Education[];
+    references: Reference[];
 }
 
 export interface Contact {
-  id: string;
-  type: ContactType;
-  data: string
+    id: string;
+    type: ContactType;
+    data: string;
 }
 
 export interface SkillGroup {
-  id: string;
-  title: string;
-  type: RequiredType;
-  data: Skill[];
+    id: string;
+    title: string;
+    type: RequiredType;
+    data: Skill[];
 }
 
 export interface Skill {
-  id: string
-  name: string;
-  type: RequiredType;
+    id: string;
+    name: string;
+    type: RequiredType;
 }
 
 export interface Education {
-  id: string;
-  university: string;
-  program: string;
-  latest: boolean;
-  type: RequiredType;
+    id: string;
+    university: string;
+    program: string;
+    latest: boolean;
+    type: RequiredType;
 }
 
 export interface Reference {
-  id: string;
-  firstName: string;
-  lastName: string;
-  designation: string;
-  mobile: string;
-  email: string;
-  type: RequiredType;
+    id: string;
+    firstName: string;
+    lastName: string;
+    designation: string;
+    mobile: string;
+    email: string;
+    type: RequiredType;
 }
 
 export enum ContactType {
-  mobile, email, teams
+    mobile,
+    email,
+    teams,
 }
 
 export enum RequiredType {
-  mandatory, standard, optional
+    mandatory,
+    standard,
+    optional,
 }
