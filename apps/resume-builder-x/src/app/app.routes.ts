d77979fe675b6bd60cd68498c008f6871ebabc10
Prasanna Raja Prabhakaran <PrasannaRaja.Prabhakaran@kpmg.co.uk>
2023-06-26 13:26:32 +0200

    Update app.routes.ts

--- apps/resume-builder-x/src/app/app.routes.ts
+++ apps/resume-builder-x/src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Route } from '@angular/router';
-import { AlphaComponent } from '@org/resume-builder-x/src/app/generate';
 
-export const appRoutes: Route[] = [
-  { path: '', pathMatch: 'full', component: AlphaComponent },
-];
+export const appRoutes: Route[] = []
