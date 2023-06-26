14d38a2313e86a7fba7f41ae82ac91ae2914e85d
Prasanna Raja <prasannaraja@msn.com>
2023-06-26 23:28:41 +0200

    shared-ui removed

--- libs/shared-ui/src/lib/pipes.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RemoveHttpPrefixPipe } from './Pipe/RemoveHttpPrefix/remove-http-prefix.pipe';
-import { HostNamePipe } from './Pipe/HostName/host-name.pipe';
-
-@NgModule({
-    declarations: [HostNamePipe, RemoveHttpPrefixPipe],
-    exports: [HostNamePipe, RemoveHttpPrefixPipe]
-})
-export class PipesModule { }
--- libs/shared-ui/src/lib/shared-ui.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PipesModule } from './pipes.module';
-
-@NgModule({
-  imports: [CommonModule, PipesModule],
-  exports: [PipesModule],
-})
-export class SharedUiModule { }
