aef0c77bd8f7bea840c10fbece441fe35aa9483e
Prasanna Raja Prabhakaran <PrasannaRaja.Prabhakaran@kpmg.co.uk>
2023-06-26 18:18:03 +0200

    Update shared-ui.module.ts

--- libs/shared-ui/src/lib/shared-ui.module.ts
+++ libs/shared-ui/src/lib/shared-ui.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HostNamePipe } from './Pipe/HostName/host-name.pipe';
-import { RemoveHttpPrefixPipe } from './Pipe/RemoveHttpPrefix/remove-http-prefix.pipe';
+import { PipesModule } from './pipes.module';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HostNamePipe, RemoveHttpPrefixPipe],
+  imports: [CommonModule, PipesModule],
+  exports: [PipesModule],
 })
-export class SharedUiModule {}
+export class SharedUiModule { }
