44ecf7fc07e41b75b47b9b989aa71635c93149d7
Prasanna Raja Prabhakaran <PrasannaRaja.Prabhakaran@kpmg.co.uk>
2023-06-26 13:26:39 +0200

    Update app.module.ts

--- apps/resume-builder-x/src/app/app.module.ts
+++ apps/resume-builder-x/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { GenerateModule } from './generate/src';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    GenerateModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
