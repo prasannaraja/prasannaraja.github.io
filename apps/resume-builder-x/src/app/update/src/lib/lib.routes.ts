ba4ec2184081591a494820a9b7fdf9d18b11a8c1
Prasanna Raja Prabhakaran <PrasannaRaja.Prabhakaran@kpmg.co.uk>
2023-06-26 13:36:37 +0200

    Update lib.routes.ts

--- apps/resume-builder-x/src/app/update/src/lib/lib.routes.ts
+++ apps/resume-builder-x/src/app/update/src/lib/lib.routes.ts
@@ -1,5 +1,8 @@
 import { Route } from '@angular/router';
 
 export const updateRoutes: Route[] = [
-  /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+  {
+    path: 'update',
+    loadChildren: () => import('./update.route.module').then(m => m.UpdateRoutingModule)
+  }
 ];
