586eac176292f9b46d6148f232f979748c3b6945
Prasanna Raja Prabhakaran <PrasannaRaja.Prabhakaran@kpmg.co.uk>
2023-06-26 13:26:22 +0200

    Update generate.module.ts

--- apps/resume-builder-x/src/app/generate/src/lib/generate.module.ts
+++ apps/resume-builder-x/src/app/generate/src/lib/generate.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { generateRoutes } from './lib.routes';
 import { AlphaComponent } from './alpha/alpha.component';
-import { SharedUiModule } from '@org/shared-ui';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedUiModule,
     RouterModule.forChild(generateRoutes),
   ],
   declarations: [AlphaComponent],
   exports: [AlphaComponent],
 })
-export class GenerateModule {}
+export class GenerateModule { }
