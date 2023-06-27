164da8db47be7be9520cf0afde1000d489d5c8ab
Prasanna Raja Prabhakaran <PrasannaRaja.Prabhakaran@kpmg.co.uk>
2023-06-27 13:00:26 +0200

    Create IExperience.ts

---
+++ apps/resume-builder-x/src/app/generate/src/models/IExperience.ts
@@ -0,0 +1,75 @@
+import { RequiredType } from "./IPersonalData";
+
+export interface IExperience {
+    companies: Company[];
+}
+
+export interface Company {
+    name: string;
+    website: string;
+    position: string;
+    summary: Summary;
+}
+
+export interface Summary {
+    company: {
+        details: string;
+    };
+    work: Work;
+}
+
+export interface Work {
+    details: string;
+    email: string;
+    manager: Manager;
+    hr: Hr;
+    employeeId: string;
+    contact: string;
+    present: boolean;
+    duration: {
+        start: {
+            month: string,
+            year: string
+        },
+        end: {
+            month: string,
+            year: string,
+        }
+    };
+    teamSize: number;
+    location: Location;
+    projects: Project[];
+    responsibilities: string[];
+    skills: string[];
+}
+
+export interface Location {
+    type: RequiredType;
+    country: string;
+    city: string;
+}
+
+export interface Manager {
+    name: string;
+    email: string;
+    mobile: string;
+    team: string;
+}
+
+export interface Hr {
+    name: string;
+    email: string;
+    mobile: string;
+}
+
+export interface Project {
+    title: string;
+    client: string;
+    website: string;
+    shortSummary: string;
+    summary: string;
+    keywords: string[];
+    responsibilities: string[];
+    skills: string[];
+    duration?: string;
+}
