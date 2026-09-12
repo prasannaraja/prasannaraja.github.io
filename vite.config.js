07e9b3bbcdf721c4f88c736dfbcaf3ac74ad7e89
Prasanna Prabhakaran <prasannaraja@msn.com>
2026-09-12 11:38:01 +0200

    localization update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/', // IMPORTANT for codermarvin.github.io
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        de: resolve(__dirname, 'de/index.html'),
+        fr: resolve(__dirname, 'fr/index.html'),
+      },
+    },
+  },
 })
